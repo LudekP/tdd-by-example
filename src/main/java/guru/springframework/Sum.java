14752f59941a68020a7b43def778b1c5f588bcaf
BALLNAZZAR-PC\Ballnazzar <ludek.p@email.cz>
2022-05-09 22:46:48 +0200

    Add sum

---
+++ src/main/java/guru/springframework/Sum.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+public class Sum {
+
+    private Money augmend;
+    private Money addmend;
+
+    public Sum(Money augmend, Money addmend) {
+        this.augmend = augmend;
+        this.addmend = addmend;
+    }
+}
